96527e67ef9374d362fa5f930dc6180cbb16b006
Eric Seigne <eric.seigne@ryxeo.com>
2012-08-30 11:29:16 +0200

    export en png format compatible nautilus / gnome / xdg / freedesktop

--- thread.cpp
+++ thread.cpp
@@ -1,3 +1,25 @@
+/** Classe   Thread
+  * @see https://redmine.ryxeo.com/projects/
+  * @author 2012 Icham Sirat <icham.sirat@ryxeo.com>
+  * @author 2012 Eric Seigne <eric.seigne@ryxeo.com>
+  * @see The GNU Public License (GNU/GPL) v3
+  *
+  *
+  *
+  * This program is free software; you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation; either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful, but
+  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+  * for more details.
+  *
+  * You should have received a copy of the GNU General Public License along
+  * with this program. If not, see <http://www.gnu.org/licenses/>.
+  */
+
 #include "thread.h"
 #include "QImageReader"
 #include "QBuffer"
@@ -98,9 +120,9 @@ void Thread::creerThumbs(QFileInfo cheminImage, QString cheminSauvegardeThumbs)
             /** Sauvegarde */
             QDir::setCurrent(cheminSauvegardeThumbs);
             QFile file;
-            file.setFileName(hashData.toHex() +"." +cheminImage.suffix());
+            file.setFileName(hashData.toHex() + ".png");
             file.open(QIODevice::WriteOnly);
-            if(thumbnail.save(&file))
+            if(thumbnail.save(&file,"PNG"))
                 qDebug() << "Ecriture du fichier OK";
         }
         else
@@ -133,9 +155,9 @@ void Thread::creerThumbs(QFileInfo cheminImage, QString cheminSauvegardeThumbs)
         /** Sauvegarde */
         QDir::setCurrent(cheminSauvegardeThumbs);
         QFile file;
-        file.setFileName(hashData.toHex() +"." +cheminImage.suffix());
+        file.setFileName(hashData.toHex() + ".png");
         file.open(QIODevice::WriteOnly);
-        if(thumbnail.save(&file))
+        if(thumbnail.save(&file, "PNG"))
             qDebug() << "Ecriture du fichier OK";
 
     }
