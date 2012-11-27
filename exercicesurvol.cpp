449070b72eb648c611905f21a86ee3337909e6a0
Icham Sirat <icham.sirat@ryxeo.com>
2012-11-27 17:25:42 +0100

    Nettoyage et commentaire .h

--- exercicesurvol.cpp
+++ exercicesurvol.cpp
@@ -109,8 +109,6 @@ ExerciceSurvol::~ExerciceSurvol()
     emit exerciceExited(); // Permet à la MainWindow de savoir que l'exercice est terminé
 }
 
-/** Charge les options contenues dans le fichier de configuration (parametres.ini)
-  */
 void ExerciceSurvol::chargerOption()
 {
     if (m_localDebug) qDebug() << "##########################  ExerciceSurvol::chargerOption()";
@@ -154,8 +152,6 @@ void ExerciceSurvol::slotSequenceEntered() // en cours
     }
 }
 
-/** Affichage de la consigne
-  */
 void ExerciceSurvol::slotPresenteSequenceEntered() //todo
 {
     if (m_localDebug) qDebug()<<"##########################  ExerciceSurvol::slotPresenteSequenceEntered()";
@@ -186,11 +182,6 @@ void ExerciceSurvol::slotPresenteSequenceEntered() //todo
     QTimer::singleShot(8000,this,SLOT(slotAppuiAutoSuivant()));     // Clic auto du bouton suivant avec un timer
 }
 
-/** Mettre tout ce qui est commun à chaque question
-  * Aller chercher le pack image
-  * Choisir 5 images au hasard dans le pack
-  * Condition de garde .abe
-  */
 void ExerciceSurvol::slotRealisationExerciceEntered()
 {
     if (m_localDebug) qDebug()<<"##########################  ExerciceSurvol::slotRealisationExerciceEntered()";
@@ -259,9 +250,6 @@ void ExerciceSurvol::slotRealisationExerciceEntered()
     }
 }
 
-/** Affichage de l'image
-  * Calcul et mise en place des masques
-  */
 void ExerciceSurvol::slotInitQuestionEntered()
 {
     if (m_localDebug) qDebug()<<"##########################  ExerciceSurvol::slotInitQuestionEntered()";
@@ -303,7 +291,7 @@ void ExerciceSurvol::slotInitQuestionEntered()
         qreal xMasque = 0.00;
         qreal yMasque = 0.00;
 
-        qDebug()<<" -------------------------- Début boucle d'affichage : "<<nbMasques;
+        if(m_localDebug) qDebug()<<" -------------------------- Début boucle d'affichage : " << nbMasques;
 
         for (float i=0; i<nbMasquesHauteur;i++)
         {
@@ -325,9 +313,6 @@ void ExerciceSurvol::slotInitQuestionEntered()
     }
 }
 
-/** Choix aléatoire du positionnement des masques interactifs
-  * Connexion du slot cacheMasque sur chaque masque interactif
-  */
 void ExerciceSurvol::slotQuestionEntered()
 {
     if (m_localDebug) qDebug()<<"##########################  ExerciceSurvol::slotQuestionEntered()";
@@ -359,9 +344,6 @@ void ExerciceSurvol::slotQuestionEntered()
     m_exerciceEnCours = true;
 }
 
-/**
-  * Appeler pour appuyer automatiquement sur le bouton suivant
-  */
 void ExerciceSurvol::slotAfficheVerificationQuestionEntered()
 {
     if (m_localDebug) qDebug()<< "##########################  ExerciceSurvol::slotAfficheVerificationQuestionEntered()";
@@ -394,7 +376,6 @@ void ExerciceSurvol::slotFinVerificationQuestionEntered()
 
 }
 
-// Ce slot est pour la derniere question !
 void ExerciceSurvol::slotFinQuestionEntered()
 {
     if (m_localDebug)qDebug()<< "##########################  ExerciceSurvol::slotFinQuestionEntered()";
@@ -407,7 +388,7 @@ void ExerciceSurvol::slotFinQuestionEntered()
     AbulEduCommonStatesV1::slotFinQuestionEntered();
 }
 
-void ExerciceSurvol::slotBilanExerciceEntered() // todo boucle pour les têtes
+void ExerciceSurvol::slotBilanExerciceEntered()
 {
     if (m_localDebug)qDebug()<< "##########################  ExerciceSurvol::slotBilanExerciceEntered()";
 
@@ -462,7 +443,7 @@ void ExerciceSurvol::slotBilanExerciceEntered() // todo boucle pour les têtes
     redimensionnerConsigne();
 }
 
-void ExerciceSurvol::slotQuitter() // ok
+void ExerciceSurvol::slotQuitter()
 {
     if (m_localDebug) qDebug()<<"##########################  ExerciceSurvol::slotQuitter";
 
@@ -504,8 +485,6 @@ void ExerciceSurvol::setDimensionsWidgets()
 //------------------------------------------------------------------
 //                 Méthodes propres à la classe
 //------------------------------------------------------------------
-/** Redimensionne la consigne
-  */
 void ExerciceSurvol::redimensionnerConsigne()
 {
     getAbeExerciceMessageV1()->abeWidgetMessageResize();
@@ -513,9 +492,6 @@ void ExerciceSurvol::redimensionnerConsigne()
                                     ((getAbeExerciceAireDeTravailV1()->height() - getAbeExerciceMessageV1()->height())/2) - 200*abeApp->getAbeApplicationDecorRatio());
 }
 
-/** Redimensionne l'image par rapport à sa largeur ou sa hauteur.
-  * Obsolète mais conserver au cas ou !
-  */
 void ExerciceSurvol::redimensionnerImage()
 {
     if (m_localDebug) qDebug()<< m_itemImage->pixmap().width()<<" " << m_itemImage->pixmap().height();
@@ -547,8 +523,6 @@ void ExerciceSurvol::redimensionnerImage()
     }
 }
 
-/** Redimensionne l'image (2e méthode)
-  */
 void ExerciceSurvol::redimensionnerImage2()
 {
     m_itemImage->setPixmap(m_itemImage->pixmap().scaled(m_tailleAireDejeu, Qt::KeepAspectRatio, Qt::SmoothTransformation));
@@ -561,11 +535,6 @@ void ExerciceSurvol::redimensionnerImage2()
                         - 60 * ratio -gv_AireDeJeu->height())/2 + 32 * ratio);
 }
 
-/** A chaque passsage sur un masque interactif, on décremente le nombre de masques interactifs.
-  * Dès que les masques interactifs sont tous survolés, on affiche l'image.
-  * On affiche la tête dans la boiteTete.
-  * On vide m_listeMasquesFixes
-  */
 void ExerciceSurvol::slotCacheMasque()
 {
     if (m_localDebug) qDebug() << "##########################  ExerciceSurvol::slotCacheMasque : " << m_nbMasquesInteractifs;
@@ -619,30 +588,18 @@ void ExerciceSurvol::slotCacheMasque()
     }
 }
 
-/** Cette méthode emet le signal appuiSuivant
-  * Permet donc d'activer (de simuler) l'appui sur le bouton suivant de la telecommande
-  */
 void ExerciceSurvol::slotAppuiAutoSuivant()
 {
     if (m_localDebug) qDebug() << "##########################  ExerciceSurvol::slotAppuiAutoSuivant()";
     emit appuiSuivant();
 }
 
-/** Cette méthode emet le signal appuiVerifier
-  * Permet donc d'activer (de simuler) l'appui sur le bouton verifier de la telecommande
-  */
 void ExerciceSurvol::slotAppuiAutoVerifier()
 {
     if (m_localDebug) qDebug() << "##########################  ExerciceSurvol::slotAppuiAutoVerifier()";
     emit appuiVerifier();
 }
 
-/** Cette méthode retourne la plus petite division d'entiers dont le résultat est supérieur à monChiffre
-  * Elle sert au calcul de la taille des masques.
-  * exemple: pour 11 masques, les divisions possibles sont 2*6 ou 3*4.
-  * le QPair retourné sera 3*4, ce qui nous donne 12.
-  * Donc à la première question, l'image sera divisé par 12 petits masques de taille identique (4 dans la largeur et 3 dans la hauteur)
-  */
 QPair<int, int> ExerciceSurvol::plusPetiteDivision(int monChiffre)
 {
     QList<QPair<int,int> > listeResultat;
@@ -689,10 +646,6 @@ QPair<int, int> ExerciceSurvol::plusPetiteDivision(int monChiffre)
     return paire;
 }
 
-/** Event Filter pour la pause.
-  * Capture l'appui sur la barre espace lorsque le booléen "onPeutMettreEnPause" est à "true".
-  * C'est le cas lorsque tous les masques sont découvert.
-  */
 bool ExerciceSurvol::eventFilter(QObject *obj, QEvent *event)
 {
     obj = this;
@@ -711,7 +664,7 @@ bool ExerciceSurvol::eventFilter(QObject *obj, QEvent *event)
             if (m_timer->isActive())
             {
                 m_timer->stop();
-                qDebug() << "Le timer est actif est vient d'etre stoppé";
+                if(m_localDebug) qDebug() << "Le timer est actif est vient d'etre stoppé";
 
                 boiteTetes->setVisible(false);
                 m_labelImagePause->show();
@@ -729,13 +682,13 @@ bool ExerciceSurvol::eventFilter(QObject *obj, QEvent *event)
             else
             {
                 m_timer->start();
-                qDebug() << "Le timer repart   ";
+                if(m_localDebug) qDebug() << "Le timer repart   ";
                 m_labelImagePause->setVisible(false);
                 m_labelTextePause->setVisible(false);
                 boiteTetes->setVisible(true);
             }
 
-            qDebug() << "Pause !";
+            if(m_localDebug) qDebug() << "Pause !";
         }
     }
     return false;
