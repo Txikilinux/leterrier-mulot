fbe1680fa1c4d6c2ea7e3b527159c8bcb4e16e5d
Icham Sirat <icham.sirat@ryxeo.com>
2013-07-26 16:34:23 +0200

    Instance unique + nettoyage comm

--- main.cpp
+++ main.cpp
@@ -26,6 +26,9 @@ int main(int argc, char *argv[])
     AbulEduApplicationV1 a(argc, argv,VER_INTERNALNAME_STR, VER_PRODUCTVERSION_STR, VER_COMPANYDOMAIN_STR, "leterrier", VER_UNITVERSION_STR);
     a.setAbeApplicationLongName(QObject::trUtf8(VER_FILEDESCRIPTION_STR));
 
+    if(a.isRunning())
+        return 0;
+
     QString locale = QLocale::system().name().section('_', 0, 0);
     QTranslator translator;
 
