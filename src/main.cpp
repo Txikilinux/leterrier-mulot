8a67761b76b5dc9dc92e23e851c922c44026508f
Icham Sirat <icham.sirat@ryxeo.com>
2014-03-14 17:32:28 +0100

    [main.cpp] Utilisation des MACROS pour AbulEduApplicationV1

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@
 
 int main(int argc, char *argv[])
 {
-    AbulEduApplicationV1 a(argc, argv,VER_INTERNALNAME_STR, VER_PRODUCTVERSION_STR, VER_COMPANYDOMAIN_STR, "leterrier", VER_UNITVERSION_STR);
+    AbulEduApplicationV1 a(argc, argv,VER_INTERNALNAME_STR, VER_PRODUCTVERSION_STR, VER_COMPANYDOMAIN_STR, VER_COMPANYNAME_STR, VER_UNITVERSION_STR);
     a.setAbeApplicationLongName(QObject::trUtf8(VER_FILEDESCRIPTION_STR));
 
     if(a.isRunning())
