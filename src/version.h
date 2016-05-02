a1132867425f239fd05195f5cb7b39419d9a7117
Icham Sirat <icham.sirat@ryxeo.com>
2016-05-02 12:41:03 +0200

    [#4817] Passage version 10.1.18

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             10,1,16,0
-#define VER_FILEVERSION_STR         "10.1.16\0"
+#define VER_FILEVERSION             10,1,18,0
+#define VER_FILEVERSION_STR         "10.1.18\0"
 
-#define VER_PRODUCTVERSION          10,1,16,0
-#define VER_PRODUCTVERSION_STR      "10.1.16\0"
+#define VER_PRODUCTVERSION          10,1,18,0
+#define VER_PRODUCTVERSION_STR      "10.1.18\0"
 
 #define VER_COMPANYNAME_STR         "RyXeo SARL"
 #define VER_FILEDESCRIPTION_STR     "AbulEdu LeTerrier -- Mulot"
